1c570cb9c6185b6da1924d265c7a3a864d9c9d7d
johnathonbegg-bsu <43049002+johnathonbegg-bsu@users.noreply.github.com>
2021-04-08 10:45:13 -0600

    Update LaserGui.py
    Added input area for frequency

--- src/LaserGui.py
+++ src/LaserGui.py
@@ -31,6 +31,7 @@ class Project(tk.Frame):
         myMenu.add_cascade(label="File", menu=menu1)
         menu1.add_command(label="Select a file", command=self.open_file)
 
+        menu1.add_command(label="Reset", command=self.reset)
         # menu1.add_command(label="Save a file", command=self.save_image)
 
         menu2 = tk.Menu(myMenu)
@@ -51,81 +52,107 @@ class Project(tk.Frame):
         # created new tabs
         self.tab = tk.Frame(self.tabPanel)
         self.tab.columnconfigure(0,weight=0)
-        self.tab.columnconfigure(3,weight=1)
-        self.tab.rowconfigure(0,weight=0)
-        #self.tab.pack(fill="both")
+        self.tab.columnconfigure(1,weight=1)
+        self.tab.columnconfigure(2,weight=1)
+        self.tab.rowconfigure(0,weight=1)
+        self.tab.pack(fill="both")
         self.tabPanel.add(self.tab,text="Tab" + str(self.tabCount))
-        #self.tabCount = self.tabCount+1
+        self.tabCount = self.tabCount+1
+        self.controlPane = tk.Frame(self.tab)
+        self.controlPane.grid(column=0,row=0)
         
-        self.label1 = tk.Label(self.tab,text="Wlax1")
-        self.Wlax1 = tk.Entry(self.tab)
-        self.label1.grid(column=0,row=0)
-        self.Wlax1.grid(column=1,row=0)
+        self.label1 = tk.Label(self.controlPane, text="Wlax1")
+        self.label1.pack()
+        self.entry1 = Entry(self.controlPane)
+        self.entry1.pack()
 
-        self.label2 = tk.Label(self.tab,text="Wlax2")
-        self.Wlax2 = tk.Entry(self.tab)
-        self.label2.grid(column=0,row=1)
-        self.Wlax2.grid(column=1,row=1)
+        self.label2 = tk.Label(self.controlPane, text="Wlax2")
+        self.label2.pack()
+        self.entry2 = Entry(self.controlPane)
+        self.entry2.pack()
 
-        self.label3 = tk.Label(self.tab,text="Taxcp")
-        self.taxcp = tk.Entry(self.tab)
-        self.label3.grid(column=0,row=2)
-        self.taxcp.grid(column=1,row=2)
-
-        btn_apply = Button(self.tab, text="Apply", command=self.tuncate_data)
-        btn_apply.grid(column=0,row=3)
-
-        btn_remove = Button(self.tab, text="Remove Tab", command=self.remove_tab)
-        btn_remove.grid(column=1,row=3)
+        self.label3 = tk.Label(self.controlPane, text="Taxcp")
+        self.label3.pack()
+        self.entry3 = Entry(self.controlPane)
+        self.entry3.pack()
+        
+        btn_apply = Button(self.controlPane, text="truncate", command=self.apply_button_action)
+        btn_apply.pack()
+        
         self.graph1 = tk.Frame(self.tab)
-        self.graph1.grid(column=2,row=0,rowspan=4,sticky="nsew")
+        self.graph2 = tk.Frame(self.tab)
+        self.graph1.grid(column=1,row=0,sticky="nsew")
+        self.graph2.grid(column=2,row=0,sticky="nsew")
 
+        self.frequencySlider = tk.Scale(self.controlPane, from_=1,to=20, orient=tk.HORIZONTAL, label="frequency")
+        self.frequencySlider.pack()
+    
     def apply_button_action(self):
-        print(self.entry.get())    
+        controlThread = Thread(target=self.truncate_graph_thread_exec, daemon = True)
+        controlThread.start()
+    
+    def truncate_graph_thread_exec(self):
+        if(isinstance(self.entry1.get(),int) & isinstance(self.entry2.get(),int) & isinstance(self.entry3.get(),int)):
+            self.fig3 = rawTAspectrum.load_truncated_chart(int(self.entry1.get()), int(self.entry2.get()), int(self.entry3.get()))
+            self.after(0,self.create_truncation_tab)
+        
+    
+    def create_truncation_tab(self):
+        self.update()
+        self.truncTab = tk.Frame(self.tabPanel)
+        self.truncTab.pack(fill="both")
+        self.tabPanel.add(self.truncTab,text="Tab" + str(self.tabCount))
+        self.tabCount = self.tabCount+1
+        self.graph3 = tk.Frame(self.truncTab)
+        self.graph3.pack()
+        canvas = FigureCanvasTkAgg(self.fig3, master=self.graph3)
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        canvas.draw()
+        self.update()
 
     #opens data file and funs analysis
     def open_file(self):
         self.fileName = filedialog.askopenfilename(initialdir="/C:", title="Select a File", filetypes=(("DAT files", "*.dat"),("All files", "*.*")))
         #rawTAspectrum.load_chart(self.fileName)
-        controlThread = Thread(target=self.start_thread, daemon=True)
+        controlThread = Thread(target=self.dat_file_thread_exec, daemon=True)
+        self.progressBar = ttk.Progressbar(root,orient=tk.HORIZONTAL,length=200,mode="indeterminate",takefocus=True,maximum=100)
         controlThread.start()
+        self.progressBar.start()
+        self.progressBar.pack()
+
     
     def get_tab_count(self):
         return self.tabCount
     
-    def start_thread(self):
+    def dat_file_thread_exec(self):
         #variable for progress bar on GUI
-        progressBar = ttk.Progressbar(root,orient=tk.HORIZONTAL,length=200,mode="indeterminate",takefocus=True,maximum=100)
-        global fig
-        progressBar.start()
-        progressBar.pack()
         self.update() 
         rawTAspectrum.load_data_file(self.fileName)
-        fig = rawTAspectrum.dummy_chart()
-        progressBar.stop()
-        progressBar.destroy()
+        self.fig1 = rawTAspectrum.load_raw_data()
+        self.fig2 = rawTAspectrum.load_data_without_axes()
         self.after(0,self.draw_plot)
     
     def draw_plot(self):
+        self.progressBar.stop()
+        self.progressBar.destroy()
         self.create_graph1()
-        canvas = FigureCanvasTkAgg(fig, master=self.graph1)
+        canvas = FigureCanvasTkAgg(self.fig1, master=self.graph1)
+        canvas2 = FigureCanvasTkAgg(self.fig2,master=self.graph2)
         canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        canvas2.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
         toolbar = NavigationToolbar2Tk(canvas, self.graph1)
+        toolbar2 = NavigationToolbar2Tk(canvas2, self.graph2)
         toolbar.update()
+        toolbar2.update()
         canvas.draw()
+        canvas2.draw()
         self.update()
-        
-    def tuncate_data(self):
-        #get input text
-        wlax1 = self.Wlax1.get()
-        wlax2 = self.Wlax2.get()
-        taxcp = self.taxcp.get()
-        #test for bad input. 
-        try:
-            float(wlax1)
-            print("float value")
-        except:
-            print("Not float")
+
+    def reset(self):
+        self.tabPanel.destroy()                             # clears out the tab panel
+        self.tabPanel = ttk.Notebook(self)                  # resetting the tab panel
+        self.tabPanel.pack(fill="both", expand=True)        
+
 
     # def save_image(self):
     #     files = [("All files", "*.*" ),
